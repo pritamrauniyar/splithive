7aaad9783465292fc253ee6af6f95094d30e273e
Pritam Rauniyar <pritamrauniyar.np@gmail.com>
2025-11-09 16:56:07 +0530

    Added necessary config for app building

--- app/app.config.js
+++ app/app.config.js
@@ -10,6 +10,30 @@ export default ({ config }) => {
 
   return {
     ...config,
+    name: config.name || 'SplitHive',
+    slug: config.slug || 'splithive',
+    version: config.version || '1.0.0',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#0f172a'
+    },
+    ios: {
+      ...(config.ios || {}),
+      bundleIdentifier: (config.ios && config.ios.bundleIdentifier) || 'com.splithive.app',
+      buildNumber: (config.ios && config.ios.buildNumber) || '1',
+      supportsTablet: true
+    },
+    android: {
+      ...(config.android || {}),
+      package: (config.android && config.android.package) || 'com.splithive.app',
+      versionCode: (config.android && config.android.versionCode) || 1,
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#0f172a'
+      }
+    },
     extra: {
       ...(config.extra || {}),
       EXPO_PUBLIC_API_BASE: apiBase,
@@ -17,4 +41,3 @@ export default ({ config }) => {
     },
   };
 };
-
